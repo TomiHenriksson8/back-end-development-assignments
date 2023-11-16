42c0de68982e61205b60d3807ac19c462750f372
TomiHenriksson8 <tomihen01@gmail.com>
2023-11-16 07:14:51 +0200

    rout-midd-db assignment

--- src/index.js
+++ src/index.js
@@ -1,77 +1,39 @@
-import http from 'http';
-import {getItems, getItemsById, postItem, putItem, deleteItem} from './items.js';
-import { fileURLToPath } from 'url';
 import express from 'express';
 import path from 'path';
-import { deleteMediaItem, getMedia, getMediaById, postMediaItem, putMediaItem } from './media.js';
-import { deleteUser, getUserById, getUsers, postUser, putUser } from './user.js';
+import {fileURLToPath} from 'url';
+import mediaRouter from './routes/media-router.mjs';
+import userRouter from './routes/user-router.mjs';
+import {logger} from './middlewares/middlewares.mjs';
 
 const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
-const __filename = fileURLToPath(import.meta.url)
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'pug');
+app.set('views', 'src/views');
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use('/docs', express.static(path.join(__dirname, '../docs')));
-app.use('/media', express.static(path.join(__dirname, '../media')));
+// Serve uploaded mediafiles url: /media/{file}
+app.use('/media', express.static(path.join(__dirname, '../uploads')));
 
+// simple custom middleware for logging/debugging all requests
+app.use(logger);
 
-app.get('/', (request, response) => {
-  const values = { title: 'Dummy REST API', message: 'TODO Docs: /docs' };
-  console.log('Rendering index page');
-  response.render('index', values, (err, html) => {
-    if (err) {
-      console.error('Error during render:', err);
-      return response.status(500).send('Error rendering the page');
-    }
-    response.send(html);
-  });
+// render pug a file (home.pug) example
+app.get('/', (req, res) => {
+  const values = {title: "Dummy REST API docs", message: "TODO: docs"};
+  res.render('home', values);
 });
 
+// media endpoints
+app.use('/api/media', mediaRouter);
 
-// Items
-
-// get all items
-app.get('/api/items', getItems);
-// get items by id
-app.get('/api/items/:id', getItemsById);
-// modify item
-app.put('/api/items', putItem);
-// add item
-app.post('/api/items', postItem);
-// delete item
-app.delete('/api/items/:id', deleteItem);
-
-// Media 
-
-// get media items
-app.get('/api/media', getMedia);
-// get media item by id
-app.get('/api/media/:id', getMediaById);
-// modify media item
-app.put('/api/media/:id', putMediaItem);
-// add media item
-app.post('/api/media', postMediaItem);
-// delete media item
-app.delete('/api/media/:id', deleteMediaItem);
-
-// User
-
-// get all users
-app.get('/api/user', getUsers);
-// get user by id
-app.get('/api/user/:id', getUserById);
-// add user
-app.post('/api/user', postUser);
-// modify user
-app.put('/api/user/:id', putUser);
-// delete user
-app.delete('/api/user/:id', deleteUser);
-
+// user endpoints
+app.use('/api/users', userRouter);
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
